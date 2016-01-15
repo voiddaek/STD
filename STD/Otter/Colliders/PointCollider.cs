216661846f7540a54c00242491ca46faddface0a
voiddaek <sebastien.b.bernier@gmail.com>
2016-01-15 22:03:04 +0100

    -reference to otter

---
+++ STD/Otter/Colliders/PointCollider.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Otter {
+    /// <summary>
+    /// Point Collider.
+    /// </summary>
+    public class PointCollider : Collider {
+
+        #region Constructors
+
+        public PointCollider(int x, int y, params int[] tags) {
+            Width = 1;
+            Height = 1;
+            X = x;
+            Y = y;
+            AddTag(tags);
+            
+        }
+
+        public PointCollider(int x, int y, Enum tag, params Enum[] tags) : this(x, y) {
+            AddTag(tag);
+            AddTag(tags);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Draw the collider for debug purposes.
+        /// </summary>
+        public override void Render() {
+            base.Render();
+
+            if (Entity == null) return;
+
+            Draw.Rectangle(Left, Top, 1, 1, Color.Red);
+        }
+
+        #endregion
+
+    }
+}
---
+++ STD/STD/Program.cs
@@ -0,0 +1,21 @@
+﻿using Otter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a game
+            var game = new Game("My First Otter Game", 640, 480, 60, false);
+
+            // Start it up
+            game.Start();
+        }
+    }
+}
